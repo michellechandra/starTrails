57b5052f09e6a2f6137b11ff4091025f8c1478b6
Jason Sigal <jason.sigal@gmail.com>
2014-06-05 17:09:19 -0400

    added music and songs rotate with the music

--- sketch.js
+++ sketch.js
@@ -10,11 +10,21 @@ var xLoc = 0; // starting x and y location of point moving around circumference 
 var yLoc = 0; 
 var degree = 0; // how far around the circle
 
+// =======================
+// variables tied to music
+// =======================
+
+// p5sound context
+var p5 = new p5Sound(this);
+var soundFile = new SoundFile('Chris_Zabriskie_-_06_-_Divider.mp3');
+var increment = 0; // map(currentTime, 0, duration, 0, 360)
+
 
 function setup () {
   createCanvas(windowWidth, windowHeight);
   background(0);
   ellipseMode(CENTER);
+  noStroke();
 
   centerX = width/2; // center of the circle
   centerY = height/2; // center of the circle
@@ -25,11 +35,14 @@ function setup () {
   for (i =0; i<=512; i++) {
     stars.push(new Star());
   }
+
+  soundFile.play();
 }
 
 function draw() { 
+  background(0, 0, 0, 2);
 
-  //radius = random(0, width/2);
+  updateIncrement();
 
   // for every Star object in the array called 'stars'...
   for (i =0; i<stars.length; i++) {
@@ -41,17 +54,16 @@ function draw() {
     stars[i].update();
   }
 
-
 }
 
 // The star object
 function Star() {
   this.color = [255, 255, 0]; // color is an array in javascript
-  // this.x = random(0, width);
-  // this.y = random(0, height);
   this.d = random(1,4); // diameter of each star ellipse
   this.degree = random(-360, 360);
-  this.radius = random(-width/2, width/2);
+  this.radius = random(-width/1.5, width/1.5);
+  this.x = centerX + (this.radius * cos(radians(this.degree)));
+  this.y = centerY + (this.radius * sin(radians(this.degree)));
 }
 
 
@@ -64,9 +76,19 @@ Star.prototype.update = function() {
 
 // called by draw loop
 Star.prototype.move = function() {
-    // update the degree
-    this.degree++;
+    this.x = centerX + (this.radius * cos(radians(this.degree + increment)));
+    this.y = centerY + (this.radius * sin(radians(this.degree + increment)));
+}
 
-    this.x = centerX + (this.radius * cos(radians(this.degree)));
-    this.y = centerY + (this.radius * sin(radians(this.degree)));
+function updateIncrement() {
+  var currentTime = soundFile.currentTime();
+  var duration = soundFile.duration();
+  console.log(increment);
+  var myIncrement = map(currentTime, 0, duration, 0, 360);
+  if (isNaN(myIncrement)) {
+    console.log('not ready');
+  }
+  else {
+    increment = myIncrement;
+  }
 }
